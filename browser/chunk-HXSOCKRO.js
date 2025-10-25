493e291678b81769a7caec9f347c53bfcd13d880
Mohamed Belal <mohamedbelal0109989@gmail.com>
2025-10-25 17:22:32 +0200

    deploy new production build

---
+++ browser/chunk-HXSOCKRO.js
@@ -0,0 +1,1 @@
+import{ab as i}from"./chunk-7JRWEJT7.js";var n=class e{transform(t,r){return t.split(" ",r).join(" ")}static \u0275fac=function(r){return new(r||e)};static \u0275pipe=i({name:"term",type:e,pure:!0})};export{n as a};
